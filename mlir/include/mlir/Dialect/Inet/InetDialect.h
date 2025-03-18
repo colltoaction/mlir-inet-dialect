25a29eac9afe2eb85d97802fe5e96b926ea22236
Martin Coll <mcoll@dc.uba.ar>
2025-03-18 21:50:06 +0000

    add to mlir/lib/Dialect

--- mlir/include/mlir/Dialect/Inet/InetDialect.h
+++ mlir/include/mlir/Dialect/Inet/InetDialect.h
@@ -1,14 +1,18 @@
-#ifndef MLIR_DIALECT_INET_INETDIALECT_H
-#define MLIR_DIALECT_INET_INETDIALECT_H
+#ifndef MLIR_DIALECT_INET_INETDIALECT_H_
+#define MLIR_DIALECT_INET_INETDIALECT_H_
 
+#include "mlir/Bytecode/BytecodeOpInterface.h"
+#include "mlir/IR/BuiltinTypes.h"
 #include "mlir/IR/Dialect.h"
+#include "mlir/IR/OpDefinition.h"
+#include "mlir/Interfaces/SideEffectInterfaces.h"
 
-//===----------------------------------------------------------------------===//
-// ODS-Generated Declarations
-//===----------------------------------------------------------------------===//
-
-#include "mlir/Dialect/Inet/Inet.h.inc"
 #include "mlir/Dialect/Inet/InetDialect.h.inc"
+
+#define GET_TYPEDEF_CLASSES
 #include "mlir/Dialect/Inet/InetTypes.h.inc"
 
-#endif // MLIR_DIALECT_INET_INETDIALECT_H
+#define GET_OP_CLASSES
+#include "mlir/Dialect/Inet/Inet.h.inc"
+
+#endif // MLIR_DIALECT_INET_INETDIALECT_H_
---
+++ mlir/lib/Dialect/Inet/InetDialect.cpp
@@ -0,0 +1,31 @@
+#include "mlir/Dialect/Inet/InetDialect.h"
+
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/BuiltinTypes.h"
+#include "mlir/IR/Dialect.h"
+#include "mlir/IR/DialectImplementation.h"
+#include "mlir/IR/OpImplementation.h"
+#include "mlir/IR/TypeUtilities.h"
+#include "mlir/Interfaces/InferTypeOpInterface.h"
+
+using namespace mlir;
+
+#include "mlir/Dialect/Inet/InetDialect.cpp.inc"
+
+void inet::InetDialect::initialize() {
+  addTypes<
+#define GET_TYPEDEF_LIST
+#include "mlir/Dialect/Inet/InetTypes.cpp.inc"
+      >();
+  addOperations<
+#define GET_OP_LIST
+#include "mlir/Dialect/Inet/Inet.cpp.inc"
+      >();
+}
+
+#define GET_OP_CLASSES
+#include "mlir/Dialect/Inet/Inet.cpp.inc"
+
+#define GET_TYPEDEF_CLASSES
+#include "mlir/Dialect/Inet/InetTypes.cpp.inc"
